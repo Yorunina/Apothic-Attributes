334f4803477af286e03db6029cba7e93173c6c78
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2024-12-21 19:04:06 -0800

    Update isPhysical to use IS_MAGIC tag

--- src/main/java/dev/shadowsoffire/apothic_attributes/util/AttributesUtil.java
+++ src/main/java/dev/shadowsoffire/apothic_attributes/util/AttributesUtil.java
@@ -2,12 +2,12 @@ package dev.shadowsoffire.apothic_attributes.util;
 
 import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageTypes;
+import net.neoforged.neoforge.common.Tags;
 
 public class AttributesUtil {
 
     public static boolean isPhysicalDamage(DamageSource src) {
-        return !src.is(DamageTypes.MAGIC) && !src.is(DamageTypes.INDIRECT_MAGIC) && !src.is(DamageTypeTags.IS_FIRE) && !src.is(DamageTypeTags.IS_EXPLOSION);
+        return !src.is(Tags.DamageTypes.IS_MAGIC) && !src.is(DamageTypeTags.IS_FIRE) && !src.is(DamageTypeTags.IS_EXPLOSION);
     }
 
 }
