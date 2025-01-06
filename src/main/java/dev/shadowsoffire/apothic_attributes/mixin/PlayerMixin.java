477b000df737d84e68f1c9c2d012291ab3d93995
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-01-06 14:21:22 -0800

    Fix MC-268917

--- src/main/java/dev/shadowsoffire/apothic_attributes/mixin/PlayerMixin.java
+++ src/main/java/dev/shadowsoffire/apothic_attributes/mixin/PlayerMixin.java
@@ -4,6 +4,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -17,4 +20,16 @@ public class PlayerMixin {
         return res || target.getPersistentData().getBoolean("apoth.killed_by_aux_dmg");
     }
 
+    /**
+     * "Fixes" MC-268917 by clamping the fall distance while sneaking to the base step height value of 0.6F instead of using the total.
+     * <p>
+     * This ensures that, while sneaking, the player may never move down further than 0.6 blocks, instead of being able to fall down an amount equal to their step
+     * height.
+     * 
+     * @apiNote "Fixes" is in quotes because Mojang has closed the bug as "Working as Intended", despite years of player feedback indicating this is a poor choice.
+     */
+    @WrapOperation(method = "maybeBackOffFromEdge", at = @At(value = "INVOKE", target = "maxUpStep()F"))
+    private float apoth_dontFallOffACliff(Player player, Operation<Float> original) {
+        return Math.min(original.call(player), 0.6F);
+    }
 }
